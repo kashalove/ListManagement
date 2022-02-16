90e16677653bee2e852e3d80714b06d81a3ae124
kashalove <89482143+kashalove@users.noreply.github.com>
2022-02-16 11:53:46 -0800

    Uploaded files

---
+++ ListManagement/Library.ListManagement/models/Item.cs
@@ -0,0 +1,39 @@
+﻿using ListManagement.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListManagement.models
+{
+    public class Item: IItem
+    {
+        private string? _name;
+        public string? Name { 
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        
+        }
+        public string? Description { get; set; }
+        public override string ToString()
+        {
+            var output = $"{Name} {Description}";
+            if (this.GetType() == typeof(ToDo))
+            { 
+                output = ((ToDo)this).ToString();
+            }
+            else if (this.GetType() == typeof(Appointment))
+            {
+                output = ((Appointment)this).ToString();
+            }
+            return output;
+        }
+    }
+}
